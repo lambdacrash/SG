616aa71622b3a97c94732aaa6e0785d7a63d003f
Lambdacrash <lambdacrash@gmail.com>
2014-03-11 14:31:51 +0100

    Dummy test

--- src/test/scala/org/lambda/graph/Graph.scala
+++ src/test/scala/org/lambda/graph/Graph.scala
@@ -1,11 +1,26 @@
 package org.lambda.graph
 
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.Matchers
+import collection.mutable.Stack
+import org.scalatest._
 
 /**
  * Created by bonfroy on 06/03/14.
  */
 class Graph extends FlatSpec with Matchers {
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    stack.pop() should be(2)
+    stack.pop() should be(1)
+  }
 
+  it should "throw NoSuchElementException if an empty stack is popped" in {
+    val emptyStack = new Stack[Int]
+    a[NoSuchElementException] should be thrownBy {
+      emptyStack.pop()
+    }
+  }
 
 }
